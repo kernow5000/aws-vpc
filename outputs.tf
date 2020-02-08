6cc98044a296f787150de73271d3e6923b344c96
Shaun Bradley <kernow5000@gmail.com>
2020-02-08 20:09:26 +0000

    vpc id output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "vpc-id" {
+  value = "${aws_vpc.vpc-tf.id}"
+}
+
 output "ngw-eip" {
   value = "${aws_eip.vpc-tf-ngw-eip}"
 }
@@ -6,7 +10,7 @@ output "pub-subnet-1-id" {
   value = "${aws_subnet.vpc-tf-pub_subnet_1.id}"
 }
 
-output "pub-subnet-1-id" {
+output "pub-subnet-2-id" {
   value = "${aws_subnet.vpc-tf-pub_subnet_2.id}"
 }
 
