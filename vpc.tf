ec53747c5515e6b5f8f24cb6126f9a9c79ec6ad8
Shaun Bradley <kernow5000@gmail.com>
2020-02-08 18:53:47 +0000

    vpc plumbing

--- vpc.tf
+++ vpc.tf
@@ -8,5 +8,48 @@ resource "aws_vpc" "vpc-tf" {
   }
 }
 
-# Create public and private subnets
+# Create two public subnets for EC2 instances etc
+resource "aws_subnet" "pub_subnet_1" {
+  vpc_id                  = aws_vpc.vpc-tf.id
+  cidr_block              = var.pub_subnet_1_cidr
+  availability_zone       = "eu-west-1a"
+  map_public_ip_on_launch = "true"
 
+  tags = {
+    Name = "Public Subnet 1"
+  }
+}
+
+resource "aws_subnet" "pub_subnet_2" {
+  vpc_id                  = aws_vpc.vpc-tf.id
+  cidr_block              = var.pub_subnet_2_cidr
+  availability_zone       = "eu-west-1b"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "Public Subnet 2"
+  }
+}
+
+# Create two private subnets for RDS instances etc (outgoing internet via NAT gateway only)
+resource "aws_subnet" "priv_subnet_1" {
+  vpc_id                  = aws_vpc.vpc-tf.id
+  cidr_block              = var.priv_subnet_1_cidr
+  availability_zone       = "eu-west-1a"
+  map_public_ip_on_launch = "false"
+
+  tags = {
+    Name = "Private Subnet 1"
+  }
+}
+
+resource "aws_subnet" "priv_subnet_2" {
+  vpc_id                  = aws_vpc.vpc-tf.id
+  cidr_block              = var.priv_subnet_2_cidr
+  availability_zone       = "eu-west-1b"
+  map_public_ip_on_launch = "false"
+
+  tags = {
+    Name = "Private Subnet 2"
+  }
+}
